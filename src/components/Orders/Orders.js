d6f1c3f6fe1dedd40f3bbb488f4ac2bc4368cd96
Mainul-Fahim <36801303+Mainul-Fahim@users.noreply.github.com>
2021-04-02 23:47:17 +0600

    done

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const Orders = () => {
    
    const{id}=useParams();
-   
+   const [orders,setOrders]=useState([]);
+   useEffect(() =>{
+    fetch(`http://localhost:5000/orders/${id}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        setOrders(data)});
+   },[id])
     return (
         <div>
-            
+            <h3>{orders.bookName}</h3>
         </div>
     );
 };
